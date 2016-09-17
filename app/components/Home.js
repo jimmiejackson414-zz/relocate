b63eb4c446605a4b6224d0f78e6858e5e572749b
Jimmie Jackson <jimmiejackson414@gmail.com>
2016-09-17 09:48:21 -0500

    initial commit

---
+++ app/components/Home.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+class Home extends Component {
+  render () {
+    return (
+      <article>
+        <div>
+          <section className='text-section'>
+            <h1>re/locate</h1>
+            <p>This application shows relevant information to a chosen city.</p>
+          </section>
+        </div>
+      </article>
+    )
+  }
+}
+
+function select (state) {
+  return {
+    data: state
+  }
+}
+
+export default connect(select)(Home)
