cc466433076612a4d2f2f8525749e205f5141e42
Jimmie Jackson <jimmiejackson414@gmail.com>
2016-09-24 14:51:19 -0500

    jquery autocomplete

--- public/assets/js/autocomplete.js
+++ public/assets/js/autocomplete.js
@@ -1,33 +1,60 @@
 // Teleport API
 
-
-
-
-
-$('#search').keydown(function(e) {
-	if(e.keyCode === 13) {
-
-		if($('#search').val() !== '') {
-			pr();
+$('#search').autocomplete({
+	serviceUrl: 'https://api.teleport.org/api/cities/',
+	paramName: 'search',
+	transformResult: function (data) {
+		console.log(data);
+		// parse data object and form into suggestions array
+		return {
+			suggestions: [
+				{
+					value: 'someString',
+					data: 'someStuff'
+				},
+				{
+					matching_full_name: 'someString2',
+					href: 'someStuff2'
+				}
+			]
 		}
-
-		else{
-			alert("Please Enter A City");
-		}
+	},
+	onSelect: function (selection) {
+		alert('You selected ' + selection.value + ' its metadata is ' + selection.data);
 	}
-
 });
 
-function pr() {
-	var city = $('#search').val().trim();
-	console.log(city);
 
 
-	var queryURL = 'https://api.teleport.org/api/cities/?search=' + city;
+// $('#search').keydown(function(e) {
+// 	if(e.keyCode === 13) {
+
+// 		if($('#search').val() !== '') {
+// 			pr();
+// 		}
+
+// 		else{
+// 			alert("Please Enter A City");
+// 		}
+// 	}
+
+// });
+
+
+// new TeleportAutocomplete({ el: '#search', maxItems: 5 });
+// TeleportAutocomplete.init('#search').on('change', function(value) { console.log(value); });
+
+
+// function pr() {
+// 	var city = $('#search').val().trim();
+// 	console.log(city);
+
+
+// 	var queryURL = 'https://api.teleport.org/api/cities/?search=' + city;
 	
-	$.ajax({ url: queryURL, method: 'GET'}).done(function(response) {
-		console.log(response);
-		console.log("We are inside api function City:" + city);	
+// 	$.ajax({ url: queryURL, method: 'GET'}).done(function(response) {
+// 		console.log(response);
+// 		console.log("We are inside api function City:" + city);	
 
-	});
-}
+// 	});
+// }
